29fd9b9c9a4ae3e34c95b8dffbbfd446ce8ef2f1
Kevin Jayne <kevin.jayne@icloud.com>
2023-03-28 06:41:25 -0400

    rm etsy

--- cmd/consts.go
+++ cmd/consts.go
@@ -3,5 +3,3 @@ package cmd
 const openai_endpoint = "https://api.openai.com/v1/"
 
 const printify_endpoint = "https://api.printify.com/v1/"
-
-const discord_channel_midjourney = "1090013272268353689"

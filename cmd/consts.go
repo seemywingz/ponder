3af95bae7af2ead9854c69a63161288133b5abfd
Kevin Jayne <kevin.jayne@icloud.com>
2023-03-29 15:42:49 -0400

    testing new opaenAI service message

--- cmd/consts.go
+++ cmd/consts.go
@@ -4,4 +4,4 @@ const openai_endpoint = "https://api.openai.com/v1/"
 
 const printify_endpoint = "https://api.printify.com/v1/"
 
-const discord_BotSystemMessage = "You are a helpful assistant Discord Chat Bot"
+const discord_BotSystemMessage = "You are Ponder. Ponder is here to help you with your Discord needs. Please be respectful and courteous when interacting with Ponder. Ponder will not tolerate any form of harassment, bullying, or discrimination. If you have any questions or concerns, please let us know. Thank you for using Ponder!"

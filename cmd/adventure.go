557a9d689c0e19e72016a6a6c5df8072c3526426
Kevin Jayne <kevin.jayne@icloud.com>
2023-08-16 21:29:59 -0400

    add more stats and format output

--- cmd/adventure.go
+++ cmd/adventure.go
@@ -4,6 +4,7 @@ Copyright © 2023 Kevin.Jayne@iCloud.com
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
@@ -18,8 +19,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-var player Character
-
 type Character struct {
 	Name        string
 	Description string
@@ -30,9 +29,42 @@ type Character struct {
 	Defense     float64
 	Dexterity   float64
 	Intellect   float64
+	Hunger      float64
 }
 
-var adventureSystemMessage = ""
+var player Character
+
+var adventureSystemMessage = `
+You are the narrator and won't accept any answers that are not relevant to the current story or anything that wasn't mentioned yet.
+In this intricate RPG world, your character's abilities are meticulously bound by the rules of reality and logical progression.
+Spells and weapon skills can only be employed if your character has undergone proper training or learning to acquire them.
+For instance, if your character hasn't previously learned a fire spell, attempting to cast one would be futile and yield no effect. 
+Similarly, wielding an unfamiliar weapon type without prior training would result in awkward and ineffective strikes.
+You cannot take any action that isn't being asked of you. 
+For example, if you choice is two paths, you cannot create a third path unless it's relevant to the current story. 
+You must choose from the options presented to you.
+
+You are a young adventurer who is just starting out on your journey. 
+You have no money, no weapons, and no armor. 
+You are wearing a simple tunic with defense 1 and trousers with defense 1. 
+You have a small pouch of coins, but not enough to buy anything useful.
+
+MP represents your magical power and recovers over time.
+HP represents your health and can be restored by drinking potions or resting. if your HP reaches 0, you will die.
+Strength represents your physical strength and affects your ability to wield weapons and armor it also increases your HP by 2 per level.
+Defense represents your ability to defend yourself from attacks and is affected by your armor.
+Dexterity represents your ability to dodge attacks and perform acrobatic feats and use ranged weapons.
+Intellect represents your ability to cast spells and use magic and increases your MP by .5 per level. 
+Hunger represents how hungry you are and affects your ability to perform strenuous tasks and is increased by strenuous tasks. 
+	hunger increases by 0.01 every action and decreases only when you eat food. if your hunger reaches 100, you will die.
+
+It's important to remember that every choice you make holds consequences. 
+Your decisions will directly shape the flow of your adventure, affecting both your immediate challenges and the unveiling of hidden secrets.
+Proceed wisely, for your path is filled with challenges and secrets yet to be unveiled. 
+The key to success lies not only in your strategic thinking but also in your adherence to the rules and limitations set by this realm.
+May your journey be both thrilling and strategic as you navigate this richly detailed realm!
+
+`
 var adventureMessages = []goai.Message{}
 
 // adventureCmd represents the adventure command
@@ -41,7 +73,6 @@ var adventureCmd = &cobra.Command{
 	Short: "lets you dive into a captivating text adventure",
 	Long:  `immerses you in a dynamic virtual story. Through text prompts, you'll make choices that lead your character through a series of challenges and decisions. Each choice you make affects the storyline's development, creating a unique and interactive narrative experience. Get ready to explore, solve puzzles, and shape the adventure's outcome entirely through your imagination and decisions.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		initAdventure()
 		startAdventure()
 	},
 }
@@ -49,44 +80,6 @@ var adventureCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(adventureCmd)
 	adventureCmd.Flags().BoolVarP(&sayText, "say", "s", false, "Say text out loud (MacOS only)")
-}
-
-func initAdventure() {
-	adventureSystemMessage = `You are the narrator and won't accept any answers that are not relevant to the current story or anything that wasn't mentioned yet.
-	In this intricate RPG world, your character's abilities are meticulously bound by the rules of reality and logical progression. Spells and weapon skills can only be employed if your character has undergone proper training or learning to acquire them.
-	For instance, if your character hasn't previously learned a fire spell, attempting to cast one would be futile and yield no effect. Similarly, wielding an unfamiliar weapon type without prior training would result in awkward and ineffective strikes.
-	You cannot take any action that isn't being asked of you. For example, if you choice is two paths, you cannot create a third path unless it's relevant to the current story. You must choose from the options presented to you.
-	It's important to remember that every choice you make holds consequences. Your decisions will directly shape the flow of your adventure, affecting both your immediate challenges and the unveiling of hidden secrets.
-	Proceed wisely, for your path is filled with challenges and secrets yet to be unveiled. The key to success lies not only in your strategic thinking but also in your adherence to the rules and limitations set by this realm.
-	May your journey be both thrilling and strategic as you navigate this richly detailed realm!
-
-	You are a young adventurer who is just starting out on your journey. 
-	You have no money, no weapons, and no armor. 
-	You are wearing a simple tunic with defense 1 and trousers with defense 1. 
-	You have a small pouch of coins, but not enough to buy anything useful.
-	
-	YOUR STARTING STATS:
-	HP: 100
-	MP: 100
-	Level: 1
-	Strength: 1
-	Defense: 2
-	Dexterity: 1
-	Intellect: 1
-
-	MP represents your magical power and recovers over time.
-	HP represents your health and can be restored by drinking potions or resting. if your HP reaches 0, you will die.
-	Strength represents your physical strength and affects your ability to wield weapons and armor.
-	Defense represents your ability to defend yourself from attacks.
-	Dexterity represents your ability to dodge attacks and perform acrobatic feats and use ranged weapons.
-	Intellect represents your ability to cast spells and use magic.
-
-	`
-
-	adventureMessages = []goai.Message{{
-		Role:    "system",
-		Content: adventureSystemMessage,
-	}}
 }
 
 func adventureChat(prompt string) string {
@@ -144,13 +137,28 @@ func adventureImage(prompt, imageFile string) {
 	}
 }
 
+func narratorSay(text string) {
+	fmt.Println("\n🗣️ : ", text)
+	if sayText {
+		say(text)
+	}
+}
+
+func getPlayerInput(player *Character) string {
+	fmt.Print("\n🗡️ " + player.Name + "🛡️ : ")
+	playerInput, err := getUserInput()
+	catchErr(err)
+	return playerInput
+}
+
 func startAdventure() {
-	fmt.Println("What is your name?")
+	narratorSay("What is your name?")
 	playerName, err := getUserInput()
 	catchErr(err)
+
 	player = Character{
 		Name:        playerName,
-		Description: "You are a young adventurer who is just starting out on your journey. You have no money, no weapons, and no armor. You are wearing a simple tunic and trousers. You have a small pouch of coins, but not enough to buy anything useful.",
+		Description: "",
 		HP:          100,
 		MP:          100,
 		Level:       1,
@@ -160,22 +168,28 @@ func startAdventure() {
 		Intellect:   1,
 	}
 
+	narratorSay("Welcome " + player.Name + ", to the world of adventure! Describe your character, be as detailed as you like.")
+	playerDescription := getPlayerInput(&player)
+	player.Description = playerDescription
+
+	playerString, err := json.Marshal(player)
+	catchErr(err)
+
+	adventureSystemMessage = adventureSystemMessage + "\n YOUR STARTING CHARACTER STATS:\n" + string(playerString)
+
+	adventureMessages = []goai.Message{{
+		Role:    "system",
+		Content: adventureSystemMessage,
+	}}
+
 	startMessage := adventureChat("My name is " + player.Name + " start adventure")
-	fmt.Println("\n🏰🗡️🛡️👑🐉🗣️ :\n", startMessage)
-	if sayText {
-		say(startMessage)
-	}
+	narratorSay(startMessage)
 	// adventureImage(startMessage, startMessage)
 
 	for {
-		fmt.Print("\n🗡️ " + player.Name + "🛡️ : ")
-		prompt, err := getUserInput()
-		catchErr(err)
-		adventureResp := adventureChat(prompt)
-		fmt.Println("\n🏰🗡️🛡️👑🐉🗣️ :\n", adventureResp)
-		if sayText {
-			say(adventureResp)
-		}
-		// adventureImage(adventureResp, adventureResp)
+		playerInput := getPlayerInput(&player)
+		adventureResponse := adventureChat(playerInput)
+		narratorSay(adventureResponse)
+		// adventureImage(adventureResponse, adventureResponse)
 	}
 }

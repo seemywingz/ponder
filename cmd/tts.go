50bd646e471dbfbfac3203b8365695ae617dd3b0
seemywings <kevin.jayne@icloud.com>
2024-04-29 23:17:17 -0400

    spk testing

--- cmd/tts.go
+++ cmd/tts.go
@@ -5,14 +5,9 @@ package cmd
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"os"
-	"time"
 
-	"github.com/faiface/beep"
-	"github.com/faiface/beep/mp3"
-	"github.com/faiface/beep/speaker"
 	"github.com/spf13/cobra"
 )
 
@@ -52,35 +47,4 @@ func tts(text string) []byte {
 		return nil
 	}
 	return audioData
-}
-
-// playAudio plays audio from a byte slice.
-func playAudio(audioContent []byte) {
-	if verbose {
-		fmt.Println("🔊 Playing audio...")
-	}
-
-	// Create an io.Reader from the byte slice
-	reader := bytes.NewReader(audioContent)
-
-	// Wrap the reader in a NopCloser to make it an io.ReadCloser.
-	readCloser := io.NopCloser(reader)
-
-	// Decode the MP3 stream.
-	streamer, format, err := mp3.Decode(readCloser)
-	catchErr(err)
-	defer streamer.Close()
-
-	// Initialize the speaker with the sample rate of the audio and a buffer size.
-	err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-	catchErr(err)
-
-	// Play the decoded audio.
-	done := make(chan bool)
-	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
-		done <- true
-	})))
-
-	// Wait for the audio to finish playing.
-	<-done
 }
--- cmd/utils.go
+++ cmd/utils.go
@@ -1,10 +1,17 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"regexp"
 	"runtime"
+	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
 )
 
 func catchErr(err error, level ...string) {
@@ -36,4 +43,59 @@ func trace() {
 	f := runtime.FuncForPC(pc[0])
 	file, line := f.FileLine(pc[0])
 	fmt.Printf("%s:%d\n%s\n", file, line, f.Name())
+}
+
+// playAudio plays audio from a byte slice.
+func playAudio(audioContent []byte) {
+	if verbose {
+		fmt.Println("🔊 Playing audio...")
+	}
+
+	// Create an io.Reader from the byte slice
+	reader := bytes.NewReader(audioContent)
+
+	// Wrap the reader in a NopCloser to make it an io.ReadCloser.
+	readCloser := io.NopCloser(reader)
+
+	// Decode the MP3 stream.
+	streamer, format, err := mp3.Decode(readCloser)
+	catchErr(err)
+	defer streamer.Close()
+
+	// Initialize the speaker with the sample rate of the audio and a buffer size.
+	err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	catchErr(err)
+
+	// Play the decoded audio.
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+
+	// Wait for the audio to finish playing.
+	<-done
+}
+
+func playMP3File(file string) {
+	if verbose {
+		fmt.Println("🔊 Playing audio file:", file)
+	}
+
+	f, err := os.Open(file)
+	catchErr(err)
+	defer f.Close()
+
+	streamer, format, err := mp3.Decode(f)
+	catchErr(err)
+	defer streamer.Close()
+
+	err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	catchErr(err)
+
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+
+	<-done
 }
